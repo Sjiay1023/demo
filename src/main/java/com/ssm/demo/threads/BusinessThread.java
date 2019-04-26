c84a928b6ad23951c121a27ab05c06e8cfec4bb6
Sjiay <Sjiay1023@163.com>
2019-04-26 14:54:21 +0800

    新增多线程模拟测试

---
+++ src/main/java/com/ssm/demo/threads/BusinessThread.java
@@ -0,0 +1,39 @@
+package com.ssm.demo.threads;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import sun.net.idn.Punycode;
+
+/**
+ *
+ */
+@Component
+@Scope("prototype")//spring多例
+public class BusinessThread implements Runnable{
+    private String acceptStr;
+    public BusinessThread(String acceptStr){
+        this.acceptStr = acceptStr;
+    }
+
+    public String getAcceptStr() {
+        return acceptStr;
+    }
+
+    public void setAcceptStr(String acceptStr) {
+        this.acceptStr = acceptStr;
+    }
+
+    @Override
+    public void run() {
+        //业务操作
+        System.out.println("多线程已经处理订单插入系统，订单号："+acceptStr);
+
+        //线程阻塞
+        /*try {
+            Thread.sleep(1000);
+            System.out.println("多线程已经处理订单插入系统，订单号："+acceptStr);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+    }
+}
